b6b4337a0357feadd1387697d6866620e18a80aa
akshata <akshata2018p@gamil.com>
2023-02-10 22:58:08 +0530

    [akshata]Add:CheckArrayLength

---
+++ ArrayProgram/Length.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayProgram
+{
+    public class Length
+    {
+        public void ArrayLength()
+        {
+            int[] array = { 4, 6, 10, 9, 12, 5 ,7 };
+            //Finding Length
+            int arrayLength = array.Length;
+            Console.WriteLine("Length of the array: "+arrayLength);
+        }
+    }
+}
---
+++ ArrayProgram/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayProgram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Length arrayLength = new Length();
+            arrayLength.ArrayLength();
+            Console.ReadLine();
+
+        }
+    }
+}
