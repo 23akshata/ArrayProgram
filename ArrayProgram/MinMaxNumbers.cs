49b94ec60fd4c80ca7d9261500fe71a95941c9e1
akshata <akshata2018p@gamil.com>
2023-02-10 23:23:57 +0530

    [akshata]Add:CheckMinMaxNumbers

---
+++ ArrayProgram/MinMaxNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayProgram
+{
+    public class MinMaxNumbers
+    {
+        public void Numbers()
+        {
+            int[] array = { 12, 23, 45, 43, 50, 54 };
+            Console.WriteLine("Minimum Number is :" + array.Min());
+            Console.WriteLine("Maximum Number is :" + array.Max());
+        }
+    }
+}
--- ArrayProgram/Program.cs
+++ ArrayProgram/Program.cs
@@ -9,11 +9,27 @@ namespace ArrayProgram
     public class Program
     {
         static void Main(string[] args)
-        {
-            Length arrayLength = new Length();
-            arrayLength.ArrayLength();
-            Console.ReadLine();
+        { 
+            Console.WriteLine("1:ArrayLength \n 2.MinMaxNumbers");
+            Console.WriteLine("Enter Your Choice:");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Length arrayLength = new Length();
+                    arrayLength.ArrayLength();
+                    break;
 
+                case 2:
+                    MinMaxNumbers numbers = new MinMaxNumbers();
+                    numbers.Numbers();
+                    break;
+
+
+            }
+            Console.ReadLine();
         }
+
+        
     }
 }
